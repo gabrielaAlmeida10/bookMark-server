b3b674cff24f8cd912364f442d002f3560f0a1d2
gabrielaAlmeida10 <136607271+gabrielaAlmeida10@users.noreply.github.com>
2024-11-04 20:54:21 -0300

    Add files via upload

---
+++ src/controllers/goalController.js
@@ -0,0 +1,11 @@
+const { addGoal } = require("../models/goalModel");
+
+const createGoal = async (req, res) => {
+  try {
+  } catch (error) {
+    res.status(500).json({ message: error.message });
+  }
+};
+
+/*
+ */
