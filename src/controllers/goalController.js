cf2ed92ecbe33e611613ea828f2803076bed9f92
gabrielaAlmeida10 <136607271+gabrielaAlmeida10@users.noreply.github.com>
2024-11-06 22:27:17 -0300

    feat: goals functions

--- src/controllers/goalController.js
+++ src/controllers/goalController.js
@@ -1,11 +1,68 @@
-const { addGoal } = require("../models/goalModel");
+const {
+  createGoal,
+  addBookToGoal,
+  updateGoalProgress,
+} = require("../models/goalModel");
 
-const createGoal = async (req, res) => {
+const createGoalController = async (req, res) => {
   try {
+    const { name, totalBooks, startDate } = req.body;
+    const userId = req.userId; // Certifique-se de que o middleware de autenticação esteja configurando `req.userId`
+
+    if (!name || !totalBooks || !startDate) {
+      return res.status(400).json({ message: "Todos os campos obrigatórios devem ser preenchidos." });
+    }
+
+    const newGoal = await createGoal({ name, totalBooks, startDate, userId });
+    res.status(201).json(newGoal);
   } catch (error) {
-    res.status(500).json({ message: error.message });
+    console.error("Erro ao criar a meta:", error.message);
+    res.status(500).json({ message: "Erro ao criar a meta: " + error.message });
+  }
+}
+
+const addBookToGoalController = async (req, res) => {
+  try {
+    const { goalId } = req.params;
+    const { book } = req.body;
+
+    if (!goalId || !book || !book.id) {
+      return res
+        .status(400)
+        .json({ message: "ID da meta e dados do livro são obrigatórios" });
+    }
+
+    await addBookToGoal(goalId, book.id);
+    res.status(200).json({ message: "Livro adicionado à meta com sucesso!" });
+  } catch (error) {
+    res
+      .status(500)
+      .json({ message: "Erro ao adicionar livro à meta: " + error.message });
   }
 };
 
-/*
- */
+
+const updateGoalProgressController = async (req, res) => {
+  try {
+    const { goalId } = req.params;
+
+    if (!goalId) {
+      return res.status(400).json({ message: "ID da meta é obrigatório!" });
+    }
+
+    await updateGoalProgress(goalId);
+    res
+      .status(200)
+      .json({ message: "Progresso da meta atualizado com sucesso!" });
+  } catch (error) {
+    res.status(500).json({
+      message: "Erro ao atualizar o progresso da meta: " + error.message,
+    });
+  }
+};
+
+module.exports = {
+  createGoalController,
+  addBookToGoalController,
+  updateGoalProgressController,
+};
--- src/models/goalModel.js
+++ src/models/goalModel.js
@@ -1,17 +1,93 @@
-const { collection, addDoc } = require("firebase/firestore");
+const {
+  collection,
+  addDoc,
+  updateDoc,
+  doc,
+  arrayUnion,
+  getDoc,
+} = require("firebase/firestore");
 const { db } = require("../firebase");
 
-const addGoal = async (goalData, userId) => {
+const createGoal = async ({ name, totalBooks, startDate, userId }) => {
   try {
-    const goalRef = await addDoc(collection(db, "goals"), {
-      ...goalData,
+    const goalData = {
+      name,
+      totalBooks,
+      startDate,
+      endDate: null,
+      status: "Not Started",
       userId,
-      createdAt: new Date(),
-    });
-    return { id: goalRef.id, ...goalData };
+      books: [],
+    };
+
+    const docRef = await addDoc(collection(db, "goals"), goalData);
+    return { id: docRef.id, ...goalData };
   } catch (error) {
-    throw new Error("Erro ao adicionar meta: " + error.message);
+    throw new Error("Erro ao salvar a meta no banco de dados: " + error.message);
   }
 };
 
-module.exports = { addGoal };
+const addBookToGoal = async (goalId, bookId) => {
+  try {
+    const goalRef = doc(db, "goals", goalId);
+    const goalSnap = await getDoc(goalRef);
+
+    if (!goalSnap.exists()) {
+      throw new Error("Meta não encontrada");
+    }
+    const goalData = goalSnap.data();
+
+    const isBookAlreadyInGoal = goalData.books.some(
+      (b) => b.bookId === bookId
+    );
+
+    if (isBookAlreadyInGoal) {
+      throw new Error("O livro já está associado a esta meta.");
+    }
+
+    const updateBooks = [
+      ...goalData.books,
+      { bookId: bookId, status: "Not Started" },
+    ];
+    await updateDoc(goalRef, { books: updateBooks });
+
+    await updateGoalProgress(goalId);
+  } catch (error) {
+    console.error("Erro ao adicionar livro à meta: ", error.message);
+    throw new Error("Erro ao adicionar livro à meta: " + error.message);
+  }
+};
+
+
+const updateGoalProgress = async (goalId) => {
+  try {
+    const goalRef = doc(db, "goals", goalId);
+    const goalSnap = await getDoc(goalRef);
+
+    if (!goalSnap.exists) {
+      throw new Error("Meta não encontrada!");
+    }
+
+    const goalData = goalSnap.data();
+    const readbooks = goalData.books.filter(
+      (book) => book.status === "read"
+    ).length;
+    const progress = (readbooks / goalData.totalBooks) * 100;
+
+    let updates = { progress };
+
+    if (goalData.status === "Not Started" && readbooks > 0) {
+      updates.status = "In Progress";
+    }
+
+    if (readbooks === goalData.totalBooks) {
+      updates.endDate = new Date();
+      updates.status = "Completed";
+    }
+    await updateDoc(goalRef, updates);
+  } catch (error) {
+    console.error("Erro ao atualizar progresso da meta: ", error.message);
+    throw new Error("Erro ao atualizar progresso da meta: " + error.message);
+  }
+};
+module.exports = { createGoal, addBookToGoal, updateGoalProgress};
