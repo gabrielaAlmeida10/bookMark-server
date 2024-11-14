04740867f316dd9de54de89c86681fefa029ac7c
gabrielaAlmeida10 <136607271+gabrielaAlmeida10@users.noreply.github.com>
2024-11-14 18:16:56 -0300

    feat: evaluations functions

---
+++ src/controllers/evaluationController.js
@@ -0,0 +1,36 @@
+const { createEvaluation,updateEvaluation } = require("../models/evaluationModel");
+
+const addEvalutationController = async (req, res) => {
+  const { bookId, rating, comments = "" } = req.body; // define valor padrão
+  const userId = req.userId;
+
+  try {
+    const result = await createEvaluation({ userId, bookId, rating, comments });
+    res.status(201).json(result);
+  } catch (error) {
+    res.status(500).json({ message: error.message });
+  }
+};
+
+
+const updateEvaluationController = async (req, res) => {
+  try {
+    const { evaluationId } = req.params;
+    const userId = req.userId; // Supondo que o middleware de autenticação injeta o `user` no request
+    const { rating, comments } = req.body;
+
+    if (!evaluationId) {
+      return res.status(400).json({ message: "ID da avaliação é obrigatório!" });
+    }
+
+    await updateEvaluation(evaluationId, userId, { rating, comments });
+
+    res.status(200).json({ message: "Avaliação atualizada com sucesso!" });
+  } catch (error) {
+    res.status(500).json({
+      message: "Erro ao atualizar a avaliação: " + error.message,
+    });
+  }
+};
+
+module.exports = { addEvalutationController, updateEvaluationController };
